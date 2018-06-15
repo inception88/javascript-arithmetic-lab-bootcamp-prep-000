eb0b999dbeca8deb2bf8d772dfb1b77fc723b592
Benjamin Jones <bljones36@gmail.com>
2018-06-15 00:38:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,13 +23,8 @@ function inc(n) {
 function dec(n) {
   return n-=1
 }
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
 
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
+
 
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
